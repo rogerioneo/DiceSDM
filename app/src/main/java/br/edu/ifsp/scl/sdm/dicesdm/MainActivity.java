d178598a70052ef22371d4497aa3b911fcbf6808
rogerioneo <rogerio.neo@gmail.com>
2019-10-05 19:08:22 -0300

    Incluindo a configuração de números de faces

--- app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/dicesdm/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.ifsp.scl.sdm.dicesdm;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Spinner numDadosSpinner;
     private ImageView resultado2ImageView;
 
+    private EditText numFacesEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,32 +45,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         numDadosSpinner = findViewById(R.id.numDadosSpinner);
         resultado2ImageView = findViewById(R.id.resultado2ImageView);
+
+        // Recuperando referência para o numFacesEditText do arquivo de layout
+        numFacesEditText = findViewById(R.id.numFacesEditText);
     }
 
     public void onClick(View view) {
         if (view.getId() == R.id.jogarDadoButton) {
             // Recuperando o número de dados selecionados
-            int numDados = Integer.parseInt(
-                    numDadosSpinner.getSelectedItem().toString());
+            int numDados =
+                    Integer.parseInt(numDadosSpinner.getSelectedItem().toString());
             // String que armazena números sorteados
             String resultadoText = "Faces sorteadas: ";
-            // Visibilidade do resultado2ImageView de acordo com número de dados
-            if (numDados == 2) {
-                resultado2ImageView.setVisibility(View.VISIBLE);
+            int numFaces;
+            try {
+                numFaces = Integer.parseInt(numFacesEditText.getText().toString());
+            } catch (NumberFormatException e) {
+                // Caso usuário não digite nenhum número de faces
+                numFaces = 6;
+            }
+            if (numFaces > 6) {
+                resultadoImageView.setVisibility(View.GONE);
+                resultado2ImageView.setVisibility(View.GONE);
             }
             else {
-                resultado2ImageView.setVisibility(View.GONE);
-                resultadoText = "Face sorteada: ";
+                resultadoImageView.setVisibility(View.VISIBLE);
+                // Visibilidade do resultado2ImageView de acordo com número de dados
+                if (numDados == 2) {
+                    resultado2ImageView.setVisibility(View.VISIBLE);
+                }
+                else {
+                    resultado2ImageView.setVisibility(View.GONE);
+                    resultadoText = "Face sorteada: ";
+                }
             }
             // Sorteando números de acordo com número de dados
             for (int i = 1; i <= numDados; i++) {
-                int resultado = geradorRandomico.nextInt(6) + 1;
+                int resultado = geradorRandomico.nextInt(numFaces) + 1;
                 resultadoText += resultado + ", ";
                 ImageView iv = (i == 1) ? resultadoImageView : resultado2ImageView;
                 setImageResource(iv, resultado);
             }
-            resultadoTextView.setText(
-                    resultadoText.substring(0, resultadoText.lastIndexOf(',')));
+            resultadoTextView.setText(resultadoText.substring(0,
+                    resultadoText.lastIndexOf(',')));
         }
     }
 
